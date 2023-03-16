55ab501d63d07867157c32a2af016a4767898ed2
Esguerra <119916216+aalliyahesguerra@users.noreply.github.com>
2023-03-16 12:16:44 +0800

    Update TryAgain.java

--- src/TryAgain.java
+++ src/TryAgain.java
@@ -6,21 +6,28 @@ import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
-public class TryAgain extends JFrame{
+public class TryAgain extends JFrame {
+    JButton soundButton;
+    String clickSound;
+    ButtonHandler bHandler = new ButtonHandler();
+    soundEffect se = new soundEffect();
+
     TryAgain(){
-        super("Snake Game");
+
+        super("Snake Game");    
         this.setLayout(new FlowLayout());
         
-
-        JLabel lb= new JLabel();
-        lb.setText("Score:");
-        
-
+        GameOverSe("wavgameover.wav");
         JLabel imageLabel = new JLabel();
         ImageIcon gameOverIcon = new ImageIcon("game over.gif");
         imageLabel.setIcon(gameOverIcon);
         this.add(imageLabel);
+
 
         JButton tryAgainButton = new JButton();
         ImageIcon tryAgainB = new ImageIcon("tryagain.png");
@@ -28,7 +35,7 @@ public class TryAgain extends JFrame{
         tryAgainButton.setBounds(425, 380, 260, 57 );
         EventHandler handler= new EventHandler();
         tryAgainButton.addActionListener(handler);
-
+        tryAgainButton.addActionListener(bHandler);
 
         JButton exitButton = new JButton();
         ImageIcon exitB = new ImageIcon("exitblue.png");
@@ -36,15 +43,10 @@ public class TryAgain extends JFrame{
         exitButton.setBounds(425, 460, 260, 57 );
         EventHandler1 handler1= new EventHandler1();
         exitButton.addActionListener(handler1);
-
+        exitButton.addActionListener(bHandler);
        // Add button to imageIcon
         imageLabel.add(tryAgainButton);
         imageLabel.add(exitButton);
-
-        // this.add(tryAgainButton);
-        // this.add(exitButton);
-        
-
 
         this.setLayout(new FlowLayout());
         this.setTitle("Snake Game");
@@ -52,11 +54,12 @@ public class TryAgain extends JFrame{
         this.setSize(1100,650);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
-        // set background color of frame
         this.getContentPane().setBackground(new Color(237,241,214));
-        // lock frame
         this.setResizable(false);
-    }
+
+        clickSound = ".//res//wavbutton.wav";
+    
+}
     private class EventHandler implements ActionListener{
         
         public void actionPerformed(ActionEvent event) {
@@ -73,6 +76,60 @@ public class TryAgain extends JFrame{
                 
             
                 dispose();
+    }
+}
+public static void GameOverSe(String path){
+    try {
+        AudioInputStream audioInputStream= AudioSystem.getAudioInputStream(new File("wavgameover.wav"));
+        Clip clip= AudioSystem.getClip();
+        clip.open(audioInputStream);
+        
+        clip.start();
+        // clip.stop();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
+public class soundEffect{
+    Clip clip;
+    
+    public void setFile(String soundFileName){
+        try{
+            File file = new File(soundFileName);
+            AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(sound);
+            
+        }
+        catch(Exception e){
+
+        }
+    }
+    public void play(){
+        clip.setFramePosition(0);
+        clip.start();
+    }
+}
+
+public class ButtonHandler implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+        se.setFile(clickSound);
+        se.play();
+
+    }
+}
+
+public static void RunStartSe(String path){
+    try {
+        AudioInputStream audioInputStream= AudioSystem.getAudioInputStream(new File("wavbutton.wav"));
+        Clip clip= AudioSystem.getClip();
+        clip.open(audioInputStream);
+        
+        clip.start();
+        // clip.stop();
+    } catch (Exception e) {
+        e.printStackTrace();
     }
 }
     }
